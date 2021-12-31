210058d4c62429f824044d00912fa3d6e5dddf35
EIGHTfs <862434889@qq.com>
2022-01-01 05:11:13 +0800

    4.0.2-5

--- src/protocol/config.js
+++ src/protocol/config.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-26 07:37:42
+ * @LastEditTime: 2022-01-01 05:04:28
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \pxrepo\src\protocol\config.js
+ */
 const Fs = require('fs-extra')
 const Path = require('path')
 
-const CONFIG_FILE_DIR = require('appdata-path').getAppDataPath('pxder')
+const CONFIG_FILE_DIR = require('appdata-path').getAppDataPath('pxrepo')
 const CONFIG_FILE = Path.resolve(CONFIG_FILE_DIR, 'protocol.json')
 
 const writeConfig = (config = { registered: false, port: 0 }) => {
