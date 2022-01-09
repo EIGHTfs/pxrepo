e95d45360759408b04580666e99e4e03d4553cfe
EIGHTfs <862434889@qq.com>
2022-01-09 19:48:16 +0800

    Update pixiv-api-client-mod.js

--- src/pixiv-api-client-mod.js
+++ src/pixiv-api-client-mod.js
@@ -79,7 +79,10 @@ function callApi(url, options, retry = 2) {
                     if (!Fs.existsSync(global.blacklistJson)) //如果不存在blacklistJson则创建
                     {
                         global.blacklist = []
-                        global.blacklist.push(new Illustrator(11))
+                        global.blacklist.push({
+                            id: 11,
+                            remarks: 'pixiv'
+                        })
                         await Fs.writeFileSync(global.blacklistJson, JSON.stringify(global.blacklist))
 
                     }
